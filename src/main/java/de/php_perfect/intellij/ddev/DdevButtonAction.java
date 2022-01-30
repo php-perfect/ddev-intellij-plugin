3c017bf975bd1384709c67b7a0a3a516ee644340
Christian Koop <contact@sprax2013.de>
2022-01-30 19:25:46 +0100

    Disables the menu buttons when `./.ddev/config.yml` does not exist

--- src/main/java/de/php_perfect/intellij/ddev/DdevButtonAction.java
+++ src/main/java/de/php_perfect/intellij/ddev/DdevButtonAction.java
@@ -2,7 +2,11 @@ package de.php_perfect.intellij.ddev;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.util.NlsActions;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -26,5 +30,32 @@ public class DdevButtonAction extends AnAction {
         }
 
         DdevExecutor.getInstance(e.getProject()).runDdev(e.getPresentation().getText(), ddevAction);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+
+        if (e.getProject() == null) {
+            e.getPresentation().setEnabled(false);
+            return;
+        }
+
+        boolean ddevConfigExists = false;
+        Module[] projectModules = ModuleManager.getInstance(e.getProject()).getModules();
+        for (Module module : projectModules) {
+            if (ddevConfigExists) {
+                break;
+            }
+
+            for (VirtualFile virtualFile : ModuleRootManager.getInstance(module).getContentRoots()) {
+                if (virtualFile.findFileByRelativePath("./.ddev/config.yml") != null) {
+                    ddevConfigExists = true;
+                    break;
+                }
+            }
+        }
+
+        e.getPresentation().setEnabled(ddevConfigExists);
     }
 }
