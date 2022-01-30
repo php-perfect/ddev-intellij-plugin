9ca3741806789c486106e5e2f592df8c5cce100e
Nico <nico.loeber94@gmail.com>
2022-01-31 00:27:52 +0100

    refactor DdevExecutor to use run window

--- src/main/java/de/php_perfect/intellij/ddev/DdevExecutor.java
+++ src/main/java/de/php_perfect/intellij/ddev/DdevExecutor.java
@@ -1,23 +1,18 @@
 package de.php_perfect.intellij.ddev;
 
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.RunContentExecutor;
 import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.filters.TextConsoleBuilderFactory;
-import com.intellij.execution.process.OSProcessHandler;
-import com.intellij.execution.process.ProcessAdapter;
-import com.intellij.execution.process.ProcessEvent;
-import com.intellij.execution.ui.ConsoleView;
-import com.intellij.execution.ui.ConsoleViewContentType;
+import com.intellij.execution.process.ColoredProcessHandler;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.process.ProcessTerminatedListener;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.wm.RegisterToolWindowTask;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowAnchor;
-import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.util.Disposer;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-import java.util.Objects;
 import java.util.WeakHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -29,57 +24,51 @@ public class DdevExecutor {
 
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
     private final Project project;
-    private final ConsoleView c;
-    private final ToolWindow toolWindow;
 
     public DdevExecutor(@NotNull Project project) {
         this.project = project;
-
-        c = TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
-
-        toolWindow = ToolWindowManager.getInstance(Objects.requireNonNull(project))
-                .registerToolWindow(new RegisterToolWindowTask("ddev", ToolWindowAnchor.BOTTOM, c.getComponent(), true,
-                        false, false, true, null, null,
-                        null));
-        toolWindow.setTitle("Log");
-        toolWindow.setToHideOnEmptyContent(false);
-        toolWindow.setAutoHide(false);
     }
 
     public void runDdev(String title, String ddevAction) {
-        executor.execute(() -> {
+        String runTitle = "DDev " + title;
+        executor.execute(() -> SwingUtilities.invokeLater(() -> {
             try {
-                SwingUtilities.invokeLater(() -> {
-                    toolWindow.setTitle(title);
-                    toolWindow.show();
-                });
-                c.clear();
-
-                GeneralCommandLine commandLine = new GeneralCommandLine("ddev", ddevAction);
-                commandLine.setCharset(StandardCharsets.UTF_8);
-                commandLine.setWorkDirectory(this.project.getBasePath());
-
-                OSProcessHandler processHandler = new OSProcessHandler(commandLine);
-                c.attachToProcess(processHandler);
-                processHandler.addProcessListener(new ProcessAdapter() {
-                    @Override
-                    public void processTerminated(@NotNull ProcessEvent e) {
-                        if (e.getExitCode() != 0) {
-                            c.print("\n\nProcess exited with code " + e.getExitCode(), ConsoleViewContentType.ERROR_OUTPUT);
-                        }
-                    }
-                });
-                processHandler.startNotify();
+                this.run(runTitle, ddevAction);
             } catch (Throwable th) {
                 Logger.getGlobal().log(Level.FINEST, "An error occurred", th);
-                c.print("\n\nAn error occurred: " + th.getMessage(), ConsoleViewContentType.ERROR_OUTPUT);
             }
-        });
+        }));
     }
 
+    public void run(String title, String action) throws ExecutionException {
+        final ProcessHandler process = this.createProcessHandler(action);
+        final RunContentExecutor runContentExecutor = new RunContentExecutor(this.project, process)
+                .withTitle(title)
+                .withFocusToolWindow(true)
+                .withActivateToolWindow(true)
+                .withStop(process::destroyProcess, () -> !process.isProcessTerminated());
+        Disposer.register(this.project, runContentExecutor);
+        runContentExecutor.run();
+    }
+
+    @NotNull
+    public ProcessHandler createProcessHandler(String ddevAction) throws ExecutionException {
+        final ProcessHandler handler = new ColoredProcessHandler(this.createCommandLine(ddevAction));
+        ProcessTerminatedListener.attach(handler);
+        return handler;
+    }
+
+    @NotNull
+    private GeneralCommandLine createCommandLine(String ddevAction) {
+        final GeneralCommandLine commandLine = new GeneralCommandLine("ddev", ddevAction);
+        commandLine.setCharset(StandardCharsets.UTF_8);
+        commandLine.setWorkDirectory(this.project.getBasePath());
+        return commandLine;
+    }
+
+    @NotNull
     public static DdevExecutor getInstance(Project project) {
         instances.computeIfAbsent(project, DdevExecutor::new);
-
         return instances.get(project);
     }
 }
