210cc3e56f1a366a44b65febe2882b7416b4d943
Nico <nico.loeber94@gmail.com>
2022-01-30 13:41:23 +0100

    Init gradle project setup

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("org.jetbrains.intellij") version "1.3.1"
+    java
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version.set("2021.3.2")
+}
+tasks {
+    patchPluginXml {
+        changeNotes.set("""
+            Add change notes here.<br>
+            <em>most HTML tags may be used</em>        """.trimIndent())
+    }
+}
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
