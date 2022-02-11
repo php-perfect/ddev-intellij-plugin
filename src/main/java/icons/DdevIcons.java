0fa174ab3be50cd39aa9d0d8b3e28e308dd89cfd
Christian Koop <contact@sprax2013.de>
2022-02-11 19:36:00 +0100

    Replaces the plugin and menu icon from the examples with a DDEV one
    Thanks to @JNSAPH for helping me with the SVG. It is based on https://ddev.com/app/themes/ddevcom_theme_2020/dist/images/ddev-logo.svg
    
    Co-authored-by: Jonas <contact@jnsaph.com>

--- src/main/java/icons/DdevIcons.java
+++ src/main/java/icons/DdevIcons.java
@@ -6,5 +6,5 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 
 public final class DdevIcons {
-    public static final @NotNull Icon DdevIcon = IconManager.getInstance().getIcon("/icons/sdk_16.svg", DdevIcons.class);
+    public static final @NotNull Icon DdevMenuIcon = IconManager.getInstance().getIcon("/icons/ddev_menu_16.svg", DdevIcons.class);
 }
