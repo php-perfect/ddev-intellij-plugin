acca720b5b444ad136b27d32950a7efbfe1f3b83
Julian Schäfer <jus@juulliian.de>
2022-03-05 16:26:07 +0100

    Use labels from Properties file

--- src/main/java/de/php_perfect/intellij/ddev/actions/DdevPredefinedTerminalAction.java
+++ src/main/java/de/php_perfect/intellij/ddev/actions/DdevPredefinedTerminalAction.java
@@ -2,6 +2,7 @@ package de.php_perfect.intellij.ddev.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
+import de.php_perfect.intellij.ddev.DdevIntegrationBundle;
 import de.php_perfect.intellij.ddev.icons.DdevIntegrationIcons;
 import de.php_perfect.intellij.ddev.terminal.DdevTerminalRunner;
 import org.jetbrains.annotations.NotNull;
@@ -10,7 +11,11 @@ import org.jetbrains.plugins.terminal.TerminalView;
 
 public class DdevPredefinedTerminalAction extends DumbAwareAction {
     public DdevPredefinedTerminalAction() {
-        super("DDEV Web Container", "Open DDEV web container terminal", DdevIntegrationIcons.DdevLogoColor);
+        super(
+                DdevIntegrationBundle.messagePointer("action.DdevIntegration.Terminal.text"),
+                DdevIntegrationBundle.messagePointer("action.DdevIntegration.Terminal.description"),
+                DdevIntegrationIcons.DdevLogoColor
+        );
     }
 
     @Override
