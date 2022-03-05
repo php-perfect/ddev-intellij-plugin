819a258641a6b081acaf978855c7fb9738e1f307
Julian Schäfer <jus@juulliian.de>
2022-03-05 16:27:08 +0100

    Only show terminal action if DDEV project is running

--- src/main/java/de/php_perfect/intellij/ddev/terminal/DdevPredefinedTerminalActionProvider.java
+++ src/main/java/de/php_perfect/intellij/ddev/terminal/DdevPredefinedTerminalActionProvider.java
@@ -3,6 +3,9 @@ package de.php_perfect.intellij.ddev.terminal;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.project.Project;
 import de.php_perfect.intellij.ddev.actions.DdevPredefinedTerminalAction;
+import de.php_perfect.intellij.ddev.cmd.Description;
+import de.php_perfect.intellij.ddev.state.DdevStateManager;
+import de.php_perfect.intellij.ddev.state.State;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.terminal.ui.OpenPredefinedTerminalActionProvider;
 
@@ -14,7 +17,23 @@ public class DdevPredefinedTerminalActionProvider implements OpenPredefinedTermi
     @Override
     public List<AnAction> listOpenPredefinedTerminalActions(@NotNull Project project) {
         List<AnAction> actions = new ArrayList<>();
-        actions.add(new DdevPredefinedTerminalAction());
+
+        State state = DdevStateManager.getInstance(project).getState();
+        Description description = state.getDescription();
+
+        if (description == null) {
+            return actions;
+        }
+
+        Description.Status status = description.getStatus();
+
+        if (status == null) {
+            return actions;
+        }
+
+        if (description.getStatus() == Description.Status.RUNNING) {
+            actions.add(new DdevPredefinedTerminalAction());
+        }
 
         return actions;
     }
