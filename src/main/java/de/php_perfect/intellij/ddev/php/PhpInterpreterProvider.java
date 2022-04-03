5df855cb88d41e92a5dc66d2865eb3b67a94d6be
Nico <nico.loeber94@gmail.com>
2022-04-03 12:37:54 +0200

    WIP: Refactor registration of PHP Interpreter
    The PHP Interpreter is currently recreated on every ddev change event
    
    #15

--- src/main/java/de/php_perfect/intellij/ddev/php/PhpInterpreterProvider.java
+++ src/main/java/de/php_perfect/intellij/ddev/php/PhpInterpreterProvider.java
@@ -1,13 +1,10 @@
 package de.php_perfect.intellij.ddev.php;
 
 import com.intellij.openapi.project.Project;
-import com.jetbrains.php.config.interpreters.PhpInterpreter;
-import de.php_perfect.intellij.ddev.cmd.Description;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public interface PhpInterpreterProvider {
-    @Nullable PhpInterpreter buildDdevPhpInterpreter(@NotNull Description description, @NotNull String composeFilePath);
+    void registerInterpreter(@NotNull DdevInterpreterConfig interpreterConfig);
 
     static PhpInterpreterProvider getInstance(@NotNull Project project) {
         return project.getService(PhpInterpreterProvider.class);
--- src/main/java/de/php_perfect/intellij/ddev/php/PhpInterpreterProviderImpl.java
+++ src/main/java/de/php_perfect/intellij/ddev/php/PhpInterpreterProviderImpl.java
@@ -7,13 +7,16 @@ import com.intellij.execution.configuration.EnvironmentVariablesData;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.PathMappingSettings;
 import com.jetbrains.php.config.interpreters.PhpInterpreter;
+import com.jetbrains.php.config.interpreters.PhpInterpretersManagerImpl;
+import com.jetbrains.php.config.interpreters.PhpInterpretersPhpInfoCacheImpl;
+import com.jetbrains.php.config.interpreters.PhpSdkAdditionalData;
+import com.jetbrains.php.config.phpInfo.PhpInfo;
+import com.jetbrains.php.config.phpInfo.PhpInfoUtil;
 import com.jetbrains.php.remote.docker.compose.PhpDockerComposeStartCommand;
 import com.jetbrains.php.remote.docker.compose.PhpDockerComposeTypeData;
 import com.jetbrains.php.remote.interpreter.PhpRemoteSdkAdditionalData;
 import com.jetbrains.php.run.remote.PhpRemoteInterpreterManager;
-import de.php_perfect.intellij.ddev.cmd.Description;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Map;
@@ -31,30 +34,65 @@ public class PhpInterpreterProviderImpl implements PhpInterpreterProvider {
         this.project = project;
     }
 
-    @Override
-    public @Nullable PhpInterpreter buildDdevPhpInterpreter(@NotNull Description description, @NotNull String composeFilePath) {
-        PhpInterpreter phpInterpreter = new PhpInterpreter();
-        phpInterpreter.setName(NAME);
-        phpInterpreter.setIsProjectLevel(true);
+    public void registerInterpreter(@NotNull DdevInterpreterConfig interpreterConfig) {
+        final PhpInterpretersManagerImpl interpretersManager = PhpInterpretersManagerImpl.getInstance(this.project);
+        PhpInterpreter interpreter = interpretersManager.findInterpreter(NAME);
 
-        final PhpRemoteSdkAdditionalData sdkData = new PhpRemoteSdkAdditionalData("php" + description.getPhpVersion());
-        sdkData.setInterpreterId(phpInterpreter.getId());
+        if (interpreter == null) {
+            interpreter = createInterpreter();
+
+            List<PhpInterpreter> interpreters = interpretersManager.getInterpreters();
+            interpreters.add(interpreter);
+            interpretersManager.setInterpreters(interpreters);
+        }
+
+        if (!this.needsUpdate(interpreter, interpreterConfig)) {
+            System.out.println("Needs no update");
+            return;
+        }
+
+        System.out.println("Updating!");
+        this.updateInterpreter(interpreter, interpreterConfig);
+        final PhpInfo phpInfo = PhpInfoUtil.getPhpInfo(this.project, interpreter, null);
+        PhpInterpretersPhpInfoCacheImpl.getInstance(this.project).setPhpInfo(interpreter.getName(), phpInfo);
+    }
+
+    private @NotNull PhpInterpreter createInterpreter() {
+        PhpInterpreter interpreter;
+        interpreter = new PhpInterpreter();
+        interpreter.setName(NAME);
+        interpreter.setIsProjectLevel(true);
+
+        return interpreter;
+    }
+
+    private boolean needsUpdate(@NotNull PhpInterpreter existingInterpreter, @NotNull DdevInterpreterConfig interpreterConfig) {
+        PhpSdkAdditionalData additionalData = existingInterpreter.getPhpSdkAdditionalData();
+
+        if (!(additionalData instanceof PhpRemoteSdkAdditionalData)) {
+            return true;
+        }
+
+        return !((PhpRemoteSdkAdditionalData) additionalData).getInterpreterPath().equals("php" + interpreterConfig.getPhpVersion());
+    }
+
+    private void updateInterpreter(@NotNull PhpInterpreter interpreter, @NotNull DdevInterpreterConfig interpreterConfig) {
+        final PhpRemoteSdkAdditionalData sdkData = new PhpRemoteSdkAdditionalData("php" + interpreterConfig.getPhpVersion());
+        sdkData.setInterpreterId(interpreter.getId());
         sdkData.setHelpersPath(HELPERS_DIR);
 
         final DockerComposeCredentialsHolder credentials = DockerComposeCredentialsType.getInstance().createCredentials();
         credentials.setAccountName(DOCKER_NAME);
-        credentials.setComposeFilePaths(List.of(composeFilePath));
+        credentials.setComposeFilePaths(List.of(interpreterConfig.getComposeFilePath()));
         credentials.setComposeServiceName(SERVICE_NAME);
         credentials.setRemoteProjectPath(DockerCredentialsEditor.DEFAULT_DOCKER_PROJECT_PATH);
-        credentials.setEnvs(EnvironmentVariablesData.create(Map.of(COMPOSE_PROJECT_NAME_ENV, "ddev-" + description.getName()), true));
+        credentials.setEnvs(EnvironmentVariablesData.create(Map.of(COMPOSE_PROJECT_NAME_ENV, "ddev-" + interpreterConfig.getName()), true));
 
         sdkData.setTypeData(new PhpDockerComposeTypeData(PhpDockerComposeStartCommand.EXEC));
         sdkData.setCredentials(DockerComposeCredentialsType.getInstance().getCredentialsKey(), credentials);
         sdkData.setPathMappings(this.loadPathMappings(sdkData));
 
-        phpInterpreter.setPhpSdkAdditionalData(sdkData);
-
-        return phpInterpreter;
+        interpreter.setPhpSdkAdditionalData(sdkData);
     }
 
     private PathMappingSettings loadPathMappings(PhpRemoteSdkAdditionalData sdkData) {
