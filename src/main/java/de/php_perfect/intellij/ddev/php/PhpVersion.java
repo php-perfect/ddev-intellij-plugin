984940f972c6781eb91e737f1698be9693bcf754
Nico <nico.loeber94@gmail.com>
2022-04-14 21:41:08 +0200

    Refactor PHPVersion class for better maintainability

--- src/main/java/de/php_perfect/intellij/ddev/php/PhpVersion.java
+++ src/main/java/de/php_perfect/intellij/ddev/php/PhpVersion.java
@@ -2,17 +2,23 @@ package de.php_perfect.intellij.ddev.php;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.project.Project;
+import de.php_perfect.intellij.ddev.PhpVersionProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public final class PhpVersion {
     private static final ExtensionPointName<de.php_perfect.intellij.ddev.PhpVersionProvider> EP_NAME = ExtensionPointName.create("de.php_perfect.intellij.ddev.phpVersionProvider");
 
     public static @Nullable String getLanguageLevelIfAvailable(@NotNull Project project) {
-        for (de.php_perfect.intellij.ddev.PhpVersionProvider versionProvider : EP_NAME.getExtensionList()) {
-            return versionProvider.getVersion(project);
+        List<PhpVersionProvider> list = EP_NAME.getExtensionList();
+
+        if (list.size() < 1) {
+            return null;
         }
 
-        return null;
+        return list.get(0).getVersion(project);
+
     }
 }
