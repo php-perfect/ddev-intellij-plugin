fa951b96372bf6feb9025e37410c14f2e1489656
Nico Löber <nico.loeber94@gmail.com>
2022-05-15 14:32:19 +0200

    Remove registering service actions in ActionManager
    Registering the project level actions in the action manager
    on application level causes conflicts.
    
    #52

--- src/main/java/de/php_perfect/intellij/ddev/serviceActions/ServiceActionManagerImpl.java
+++ src/main/java/de/php_perfect/intellij/ddev/serviceActions/ServiceActionManagerImpl.java
@@ -1,50 +1,42 @@
 package de.php_perfect.intellij.ddev.serviceActions;
 
-import com.google.common.collect.Maps;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.extensions.PluginId;
 import com.intellij.serviceContainer.NonInjectable;
 import de.php_perfect.intellij.ddev.DdevIntegrationBundle;
 import de.php_perfect.intellij.ddev.actions.OpenServiceAction;
 import de.php_perfect.intellij.ddev.cmd.Description;
 import de.php_perfect.intellij.ddev.cmd.Service;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.TestOnly;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.AbstractMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.TestOnly;
 
 public final class ServiceActionManagerImpl implements ServiceActionManager, Disposable {
-
-    private static final PluginId PLUGIN_ID = PluginId.findId("de.php_perfect.intellij.ddev");
     private static final @NotNull Logger LOGGER = Logger.getLogger(ServiceActionManagerImpl.class.getName(),
-        DdevIntegrationBundle.getName());
+            DdevIntegrationBundle.getName());
     private static final @NotNull String ACTION_PREFIX = "DdevIntegration.Services.";
 
     private final @NotNull Map<@NotNull String, @NotNull AnAction> actionMap;
 
-    private final Supplier<ActionManager> actionManagerSupplier;
-
     public ServiceActionManagerImpl() {
-        this(ActionManager::getInstance, new ConcurrentHashMap<>());
+        this(new ConcurrentHashMap<>());
     }
 
     @NonInjectable
     @TestOnly
-    public ServiceActionManagerImpl(Supplier<ActionManager> actionManagerSupplier,
-        Map<@NotNull String, @NotNull AnAction> existingActionsMap) {
-        this.actionManagerSupplier = actionManagerSupplier;
+    public ServiceActionManagerImpl(@NotNull Map<@NotNull String, @NotNull AnAction> existingActionsMap) {
         this.actionMap = existingActionsMap;
     }
 
@@ -59,17 +51,11 @@ public final class ServiceActionManagerImpl implements ServiceActionManager, Dis
         }
 
         final Map<@NotNull String, @NotNull AnAction> newActionsMap = description.getServices()
-            .entrySet()
-            .stream()
-            .map(this::mapToServiceNameWithAction)
-            .flatMap(Optional::stream)
-            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-        var diff = Maps.difference(this.actionMap, newActionsMap);
-
-        var actionManager = this.actionManagerSupplier.get();
-        diff.entriesOnlyOnLeft().forEach((key, value) -> actionManager.unregisterAction(key));
-        diff.entriesOnlyOnRight().forEach((key, value) -> actionManager.registerAction(key, value, PLUGIN_ID));
+                .entrySet()
+                .stream()
+                .map(this::mapToServiceNameWithAction)
+                .flatMap(Optional::stream)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
         this.actionMap.clear();
         this.actionMap.putAll(newActionsMap);
@@ -77,14 +63,14 @@ public final class ServiceActionManagerImpl implements ServiceActionManager, Dis
 
     // Map.Entry<ServiceName, AnAction>
     private Optional<Map.Entry<String, AnAction>> mapToServiceNameWithAction(
-        Map.Entry<String, Service> serviceNameToActionEntry) {
+            Map.Entry<String, Service> serviceNameToActionEntry) {
         String fullName = serviceNameToActionEntry.getValue().getFullName();
         URL url;
         try {
             url = extractServiceUrl(serviceNameToActionEntry.getValue());
         } catch (MalformedURLException exception) {
             LOGGER.log(Level.WARNING,
-                String.format("Skipping open action for service %s because of its invalid URL", fullName), exception);
+                    String.format("Skipping open action for service %s because of its invalid URL", fullName), exception);
             return Optional.empty();
         }
 
--- src/test/java/de/php_perfect/intellij/ddev/serviceActions/ServiceActionManagerImplTest.java
+++ src/test/java/de/php_perfect/intellij/ddev/serviceActions/ServiceActionManagerImplTest.java
@@ -1,14 +1,6 @@
 package de.php_perfect.intellij.ddev.serviceActions;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.util.Arrays.array;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
 import com.intellij.icons.AllIcons;
-import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import de.php_perfect.intellij.ddev.actions.OpenServiceAction;
 import de.php_perfect.intellij.ddev.cmd.DatabaseInfo;
@@ -16,12 +8,16 @@ import de.php_perfect.intellij.ddev.cmd.DatabaseInfo.Type;
 import de.php_perfect.intellij.ddev.cmd.Description;
 import de.php_perfect.intellij.ddev.cmd.Description.Status;
 import de.php_perfect.intellij.ddev.cmd.Service;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Arrays.array;
 
 class ServiceActionManagerImplTest {
 
@@ -29,36 +25,17 @@ class ServiceActionManagerImplTest {
     @DisplayName("should update actions from given description")
     void testUpdateActionsByDescription() {
         var serviceActionManager = new ServiceActionManagerImpl(
-            () -> mock(ActionManager.class),
-            new HashMap<>(Map.of(
-                "existingAction",
-                anAction("Open existing action", "https://www.existing.com", "existing action")
-            )));
+                new HashMap<>(Map.of(
+                        "existingAction",
+                        anAction("Open existing action", "https://www.existing.com", "existing action")
+                )));
 
         serviceActionManager.updateActionsByDescription(aDescription("test", Status.RUNNING));
 
         assertThat(serviceActionManager.getServiceActions()).isEqualTo(array(
-            aMailhogAction(),
-            anAction("Open test", "https://www.test.com", "Open test service in your browser")
+                aMailhogAction(),
+                anAction("Open test", "https://www.test.com", "Open test service in your browser")
         ));
-    }
-
-    @Test
-    @DisplayName("should update actions registering / unregistering")
-    void testUpdateActionsByDescriptionCallsRegisterUnregister() {
-        var actionManager = mock(ActionManager.class);
-        var serviceActionManager = new ServiceActionManagerImpl(
-            () -> actionManager,
-            new HashMap<>(Map.of(
-                "existingAction",
-                anAction("Open existing action", "https://www.existing.com", "existing action")
-            )));
-
-        serviceActionManager.updateActionsByDescription(aDescription("test", Status.RUNNING));
-
-        verify(actionManager).unregisterAction("existingAction");
-        verify(actionManager).registerAction(eq("DdevIntegration.Services.ddev-config-test-mailhog"), any(), any());
-        verify(actionManager).registerAction(eq("DdevIntegration.Services.test"), any(), any());
     }
 
     private AnAction anAction(String displayText, String url, String description) {
@@ -71,19 +48,16 @@ class ServiceActionManagerImplTest {
 
     private AnAction aMailhogAction() {
         return anAction("Open Mailhog", "https://www.test.com",
-            "Open ddev-config-test-mailhog service in your browser");
+                "Open ddev-config-test-mailhog service in your browser");
     }
 
     private Description aDescription(String name, Description.Status status) {
         var dataBaseInfo = new DatabaseInfo(Type.MYSQL, "5.7", 2133, "db", "localhost",
-            "root", "root", 2133);
+                "root", "root", 2133);
 
         var httpUrl = String.format("http://www.%s.com", name);
         var httpsUrl = String.format("https://www.%s.com", name);
 
-        return new Description(name, "7.4", status, httpsUrl, httpUrl,
-            Map.of(
-                name, new Service(name, httpsUrl, httpUrl)
-            ), dataBaseInfo);
+        return new Description(name, "7.4", status, httpsUrl, httpUrl, Map.of(name, new Service(name, httpsUrl, httpUrl)), dataBaseInfo);
     }
 }
