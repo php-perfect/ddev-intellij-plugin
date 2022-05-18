fac9e1eb26f5f96b8e6078baf95c4f6a435ce3f2
Nico Löber <nico.loeber94@gmail.com>
2022-05-18 22:33:29 +0200

    Add more reliable check for optional plugin dependencies
    #36

--- src/main/java/de/php_perfect/intellij/ddev/php/ConfigurationProviderImpl.java
+++ src/main/java/de/php_perfect/intellij/ddev/php/ConfigurationProviderImpl.java
@@ -1,5 +1,7 @@
 package de.php_perfect.intellij.ddev.php;
 
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import de.php_perfect.intellij.ddev.cmd.Description;
@@ -7,7 +9,15 @@ import de.php_perfect.intellij.ddev.notification.DdevNotifier;
 import de.php_perfect.intellij.ddev.settings.DdevSettingsState;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public final class ConfigurationProviderImpl implements ConfigurationProvider {
+    private static final List<String> REQUIRED_PLUGINS = List.of(
+            "org.jetbrains.plugins.phpstorm-remote-interpreter",
+            "org.jetbrains.plugins.phpstorm-docker",
+            "Docker"
+    );
+
     private final @NotNull Project project;
 
     public ConfigurationProviderImpl(@NotNull Project project) {
@@ -30,11 +40,15 @@ public final class ConfigurationProviderImpl implements ConfigurationProvider {
             return;
         }
 
-        try {
-            Class.forName("com.intellij.docker.remote.DockerComposeCredentialsType");
-        } catch (ClassNotFoundException e) {
-            DdevNotifier.getInstance(this.project).asyncNotifyMissingPlugin("PHP Docker");
-            return;
+        final var pluginManager = PluginManager.getInstance();
+
+        for (final String id : REQUIRED_PLUGINS) {
+            final PluginId pluginId = PluginId.findId(id);
+
+            if (pluginId == null || pluginManager.findEnabledPlugin(pluginId) == null) {
+                DdevNotifier.getInstance(this.project).asyncNotifyMissingPlugin(id);
+                return;
+            }
         }
 
         final DdevInterpreterConfig ddevInterpreterConfig = new DdevInterpreterConfig(description.getName(), "php" + description.getPhpVersion(), composeFile.getPath());
