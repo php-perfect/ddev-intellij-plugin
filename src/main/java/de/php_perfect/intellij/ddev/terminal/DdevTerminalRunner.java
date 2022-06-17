6e8637202c9484b05535b19ff0d17d7f9dac9a9a
Nico Löber <nico.loeber94@gmail.com>
2022-06-18 00:22:52 +0200

    Add missing @Override annotation to isTerminalSessionPersistent

--- src/main/java/de/php_perfect/intellij/ddev/terminal/DdevTerminalRunner.java
+++ src/main/java/de/php_perfect/intellij/ddev/terminal/DdevTerminalRunner.java
@@ -167,6 +167,7 @@ public final class DdevTerminalRunner extends AbstractTerminalRunner<PtyProcess>
         }
     }
 
+    @Override
     public boolean isTerminalSessionPersistent() {
         return false;
     }
