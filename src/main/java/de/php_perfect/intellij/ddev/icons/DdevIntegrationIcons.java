96fc41bd28f0a7ad3859ae9b906656272033294b
Nico Löber <nico.loeber94@gmail.com>
2022-06-18 00:29:33 +0200

    Add private constructor for DdevIntegrationIcons to avoid instantiation

--- src/main/java/de/php_perfect/intellij/ddev/icons/DdevIntegrationIcons.java
+++ src/main/java/de/php_perfect/intellij/ddev/icons/DdevIntegrationIcons.java
@@ -8,4 +8,7 @@ import javax.swing.*;
 public final class DdevIntegrationIcons {
     public static final @NotNull Icon DdevLogoColor = IconManager.getInstance().getIcon("/icons/ddevLogoColor.svg", DdevIntegrationIcons.class);
     public static final @NotNull Icon DdevLogoMono = IconManager.getInstance().getIcon("/icons/ddevLogoGrey.svg", DdevIntegrationIcons.class);
+
+    private DdevIntegrationIcons() {
+    }
 }
