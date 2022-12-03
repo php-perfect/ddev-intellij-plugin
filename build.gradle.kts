bfb2d5c1b24fd4471deb57fbfadc86d9c5366554
Christian Koop <contact@sprax2013.de>
2022-12-03 22:44:29 +0100

    Fix failing unit tests by adding `junit-platform-launcher` as dependency
    Probably caused by a change in the resulting dependency tree.
    This fixes a `NoClassDefFoundError` that occurs when trying to run the tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.1")
     testImplementation("org.mockito:mockito-core:4.9.0")
     testImplementation("org.assertj:assertj-core:3.23.1")
 }
