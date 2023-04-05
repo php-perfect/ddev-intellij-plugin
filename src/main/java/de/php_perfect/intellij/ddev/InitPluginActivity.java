1fbbcd551a127ed88769b4155f0626ca5c9bb0f6
Nico Löber <nico.loeber94@gmail.com>
2023-04-05 11:52:38 +0200

    Migrate InitPluginActivity for compatibility with intellij 2023.1

--- src/main/java/de/php_perfect/intellij/ddev/InitPluginActivity.java
+++ src/main/java/de/php_perfect/intellij/ddev/InitPluginActivity.java
@@ -1,16 +1,22 @@
 package de.php_perfect.intellij.ddev;
 
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.startup.ProjectActivity;
 import de.php_perfect.intellij.ddev.errorReporting.SentrySdkInitializer;
 import de.php_perfect.intellij.ddev.state.DdevStateManager;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public final class InitPluginActivity implements StartupActivity, StartupActivity.DumbAware {
+public final class InitPluginActivity implements ProjectActivity, DumbAware {
+    @Nullable
     @Override
-    public void runActivity(@NotNull Project project) {
+    public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
         SentrySdkInitializer.init();
         ApplicationManager.getApplication().executeOnPooledThread(() -> DdevStateManager.getInstance(project).initialize());
+        return null;
     }
 }
