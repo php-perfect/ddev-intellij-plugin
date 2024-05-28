9394705ce62765c9726ebfd961af6cabbed9a607
Nico Löber <nico.loeber94@gmail.com>
2024-05-28 16:36:13 +0200

    Upgrade to v2 of the intellij platform gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,28 @@
 import org.jetbrains.changelog.Changelog
-import org.jetbrains.intellij.tasks.ListProductsReleasesTask
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.tasks.VerifyPluginTask
+
+fun properties(key: String) = providers.gradleProperty(key)
+fun environment(key: String) = providers.environmentVariable(key)
 
 plugins {
     id("org.jetbrains.changelog") version "2.2.0"
-    id("org.jetbrains.intellij") version "1.17.3"
+    id("org.jetbrains.intellij.platform.migration") version "2.0.0-beta4"
     id("java")
 
     id("org.sonarqube") version "5.0.0.4638"
     id("jacoco")
 }
 
-group = "de.php_perfect.intellij.ddev"
+group = properties("pluginGroup").get()
 version = System.getenv("GIT_TAG_NAME") ?: "0.0.1-dev"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
@@ -29,6 +37,36 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
     testImplementation("org.mockito:mockito-core:5.12.0")
     testImplementation("org.assertj:assertj-core:3.25.3")
+
+    intellijPlatform {
+        phpstorm(properties("platformVersion"))
+        pluginVerifier()
+        zipSigner()
+        instrumentationTools()
+        testFramework(TestFrameworkType.Platform.JUnit4)
+
+        bundledPlugins(
+            "com.intellij.database",
+            "org.jetbrains.plugins.terminal",
+            "com.jetbrains.plugins.webDeployment",
+            "com.jetbrains.plugins.webDeployment",
+            "org.jetbrains.plugins.remote-run",
+            "Docker",
+            "NodeJS",
+            "org.jetbrains.plugins.node-remote-interpreter"
+        )
+
+        /**
+         * @link https://plugins.jetbrains.com/plugin/6610-php/versions
+         * @link https://plugins.jetbrains.com/plugin/7511-php-remote-interpreter/versions
+         * @link https://plugins.jetbrains.com/plugin/8595-php-docker/versions
+         * */
+        plugins(
+            "com.jetbrains.php:241.14494.240",
+            "org.jetbrains.plugins.phpstorm-remote-interpreter:241.14494.158",
+            "org.jetbrains.plugins.phpstorm-docker:241.14494.158"
+        )
+    }
 }
 
 java {
@@ -37,46 +75,43 @@ java {
     }
 }
 
-// See https://github.com/JetBrains/gradle-intellij-plugin/
-intellij {
-    type.set("IU")
-    version.set("2024.1") // https://www.jetbrains.com/de-de/idea/download/other.html
-    plugins.add("com.intellij.database") // bundled
-    plugins.add("org.jetbrains.plugins.terminal") // bundled
-    plugins.add("com.jetbrains.plugins.webDeployment") // bundled
-    plugins.add("org.jetbrains.plugins.remote-run") // bundled
-    plugins.add("Docker") // bundled
-    plugins.add("NodeJS") // bundled
-    plugins.add("org.jetbrains.plugins.node-remote-interpreter") // bundled
-    plugins.add("com.jetbrains.php:241.14494.240") // https://plugins.jetbrains.com/plugin/6610-php/versions
-    plugins.add("org.jetbrains.plugins.phpstorm-remote-interpreter:241.14494.158") // https://plugins.jetbrains.com/plugin/7511-php-remote-interpreter/versions
-    plugins.add("org.jetbrains.plugins.phpstorm-docker:241.14494.158") // https://plugins.jetbrains.com/plugin/8595-php-docker/versions
-}
-
-tasks {
-    patchPluginXml {
+intellijPlatform {
+    pluginConfiguration {
+        name = properties("pluginName")
         changeNotes.set(provider {
-            changelog.getOrNull(version.get())
+            changelog.getOrNull(version.toString())
                 ?.let { changelog.renderItem(it, Changelog.OutputType.HTML) }
         })
     }
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN") ?: "")
-        privateKey.set(System.getenv("PRIVATE_KEY") ?: "")
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD") ?: "")
-    }
-
-    publishPlugin {
-        token.set(System.getenv("JETBRAINS_TOKEN") ?: "")
+    publishing {
+        token.set(environment("JETBRAINS_TOKEN"))
         if (System.getenv("PUBLISH_CHANNEL") != null && System.getenv("PUBLISH_CHANNEL") != "") {
             channels.set(listOf(System.getenv("PUBLISH_CHANNEL")))
         }
     }
 
-    listProductsReleases {
-        types.set(listOf("IC", "IU", "PS", "WS", "DB"))
-        releaseChannels.set(listOf(ListProductsReleasesTask.Channel.RELEASE))
+    signing {
+        certificateChain.set(environment("CERTIFICATE_CHAIN"))
+        privateKey.set(environment("PRIVATE_KEY"))
+        password.set(environment("PRIVATE_KEY_PASSWORD"))
+    }
+
+    verifyPlugin {
+        ignoredProblemsFile = file("ignoredProblems.txt")
+        failureLevel.set(VerifyPluginTask.FailureLevel.NONE)
+        ides {
+            ide(IntelliJPlatformType.PhpStorm, "2024.1")
+            ide(IntelliJPlatformType.WebStorm, "2024.1")
+            ide(IntelliJPlatformType.DataGrip, "2024.1")
+            ide(IntelliJPlatformType.IntellijIdeaUltimate, "2024.1")
+        }
+    }
+}
+
+tasks {
+    wrapper {
+        gradleVersion = properties("gradleVersion").get()
     }
 
     /* Tests */
